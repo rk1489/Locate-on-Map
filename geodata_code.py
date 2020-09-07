35ece3f7165ec11b4fa0cb00ff9bae499261d408
Raushan Kumar <69127358+rk1489@users.noreply.github.com>
2020-09-07 14:09:47 +0530

    Update geodata_code.py

--- geodata_code.py
+++ geodata_code.py
@@ -17,42 +17,47 @@ ctx.verify_mode = ssl.CERT_NONE
 conn = sqlite3.connect('geodata_index.sqlite')
 cur = conn.cursor()
 
-#Delete any existing table
+#Delete any existing table 
 cur.execute('''DROP TABLE IF EXISTS Locations''')
 
 #Create a new table
 cur.execute('''CREATE TABLE Locations (address TEXT, geodata TEXT)''')
 
-#Take the no. and name of locations as input
-try:
-    no_of_locations = int(input("Enter the no. of places to locate on map: "))
-except:
-    print("ERROR, You didn't enter an integer.")
-    no_of_locations = 0
+
+#----------Function to take input values----------
+def input_values():
+    #Take the no. and name of locations as input
+    try:
+        no_of_locations = int(input("Enter the no. of places to locate on map: "))
+    except:
+        print("ERROR, You didn't enter an integer.")
+        no_of_locations = 0
+
+    list_of_locations = []
+
+    if no_of_locations==1:
+        print("Now, Enter the name of the place:")
+    elif no_of_locations>1:
+        print("Now, Enter the name of the places:")
+
+    for _ in range(no_of_locations):
+        location_name = input()
+        list_of_locations.append(location_name)
+
+    print('')
     
-list_of_locations = []
+    return list_of_locations
+#-----------------------------------------------------------------------------------------------
 
-if no_of_locations==1:
-    print("Now, Enter the name of the place:")
-elif no_of_locations>1:
-    print("Now, Enter the name of the places:")
-
-for _ in range(no_of_locations):
-    location_name = input()
-    list_of_locations.append(location_name)
     
-print('')
-
-
-#Retrieve the geodata for each location in the list and insert into the table
-for location_name in list_of_locations:
-
+#----------Function to retrieve the geodata----------
+def retrieve_data(location_name):
     address = location_name.strip()
 
     parameters = dict()
     parameters["address"] = address
     parameters['key'] = api_key
-    
+
     #Generate the url using urlencode
     url = api_serviceurl + urllib.parse.urlencode(parameters)
 
@@ -61,87 +66,117 @@ for location_name in list_of_locations:
         fhand = urllib.request.urlopen(url, context=ctx)
     except:
         print("Device not connected to Internet.\n")
-        break
+        return ['break',0]
 
     print('Retrieving', url)
     
     #Read and decode the geodata
     fhand = fhand.read()
     geodata = fhand.decode()
-    
+
     print('Retrieved', len(geodata), 'characters')
 
     #Check for Unicode errors
     try:
         js = json.loads(geodata)
-        
+
         #Check for correct response from the api_serviceurl
         if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') :
             print('======= Failed To Retrieve The Geodata =======')
             print(geodata)
-            break
-        
+            return ['break',0]
+
         print(geodata)
-        
+
     except:
         print(geodata)
-        continue
+        return ['continue',0]
 
     print('')
-    
-    #Insert the geodata with 'OK' status to the table 
+
+    #Return the geodata with 'OK' status to the main function
     if ('status' in js and js['status'] == 'OK'):
-        cur.execute('''INSERT INTO Locations (address, geodata)
+        return [address,geodata]
+    else:
+        return ['continue',0]
+#-----------------------------------------------------------------------------------------------
+   
+
+#----------Function to insert the geodata into the table----------
+def insert_data(address,geodata):
+    cur.execute('''INSERT INTO Locations (address, geodata)
             VALUES ( ?, ? )''', (memoryview(address.encode()), memoryview(geodata.encode()) ) )
-        conn.commit()
-        
+    conn.commit()
+#-----------------------------------------------------------------------------------------------
 
-#Query the table for the geodata
-cur.execute('SELECT * FROM Locations')
 
-#Open the Javascript file
-fhand = codecs.open('geodata_locate.js', 'w', "utf-8")
+#----------Function to create the javascript file----------
+def write_on_js():
+    #Query the geodata from the table
+    cur.execute('SELECT * FROM Locations')
 
-fhand.write("myData = [\n")
-
-count = 0
-
-#Write the geodata to the Javascript file
-for row in cur :
-    data = str(row[1].decode())
-
-    #Check for Unicode errors
-    try:
-        js = json.loads(str(data))
-    except:
-        continue
-
-    latitude = js["results"][0]["geometry"]["location"]["lat"]
-    longitude = js["results"][0]["geometry"]["location"]["lng"]
+    #Open the Javascript file
+    fhand = codecs.open('geodata_locate.js', 'w', "utf-8")
     
-    if latitude == 0 or longitude == 0 :
-        continue
-    
-    location = js['results'][0]['formatted_address']
-    location = location.replace("'", "")
-    
-    try :
-        print(location, latitude, longitude)
-        print('')
-        count = count + 1
-        if count > 1 : fhand.write(",\n")
-        output = "["+str(latitude)+","+str(longitude)+", '"+location+"']"
-        fhand.write(output)
-    except:
-        continue
+    fhand.write("myData = [\n")
 
-fhand.write("\n];\n")
+    count = 0
 
+    #Write the geodata to the Javascript file
+    for row in cur :
+        data = str(row[1].decode())
+
+        #Check for Unicode errors
+        try:
+            js = json.loads(str(data))
+        except:
+            continue
+
+        latitude = js["results"][0]["geometry"]["location"]["lat"]
+        longitude = js["results"][0]["geometry"]["location"]["lng"]
+
+        if latitude == 0 or longitude == 0 :
+            continue
+
+        location = js['results'][0]['formatted_address']
+        location = location.replace("'", "")
+        try :
+            print(location, latitude, longitude)
+            print('')
+            count = count + 1
+            if count > 1 : fhand.write(",\n")
+            output = "["+str(latitude)+","+str(longitude)+", '"+location+"']"
+            fhand.write(output)
+        except:
+            continue
+    fhand.write("\n];\n")
+
+    #Close the Javascript file
+    fhand.close()
+
+    return count
+#-----------------------------------------------------------------------------------------------
+
+
+#----------Main Function----------
+#Call the 'input_values' function to take input
+list_of_locations=input_values()
+
+#Retrieve the geodata for each location in the list and insert into the table
+for location_name in list_of_locations:
+    address,geodata = retrieve_data(location_name)
+    if address=='break':
+        break
+    elif address=='continue':
+        continue
+    else:
+        insert_data(address,geodata)
+
+#Call the 'write_on_js' function
+count=write_on_js()
+ 
 #Close the sqlite database
 cur.close()
-
-#Close the Javascript file
-fhand.close()
 
 if count==1:
     print(count, "place located on Geodata map.")
@@ -153,4 +188,4 @@ else:
 if count>0:
     print("Open geodata_map.html to view the Geodata map.")
 
-#====================End of Program=====================
+#=======================End of Program========================
